e627de2e557d2bd3222f1f4287b893ff30208c01
Pal <Pal@LAPTOP-EI331EBV.mynet>
2019-04-28 07:39:05 +0100

    MVC fully working registration and login; linked with latest version of the database updated by Kim.

--- views/users/register.php
+++ views/users/register.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
-<html>
-      <body>
-    <H1>Please Login</H1>
-
+<div class="wrapper">
+    <h2>Sign Up</h2>
+    <p>Please fill this form to create an account.</p>
     <div class="container" >
         <div class="row box" id="login-box">
             <div class="col-md-6 col-md-offset-3" >
@@ -11,7 +10,13 @@
                         <div class="row " >
                             <div class="col-lg-12" >
                                 <div class="alert alert-danger" role="alert" id="error" style="display: none;">...</div>
-                                <form id="loginForm" name="loginForm" role="form" style="display: block;" method="post" action="index.php?controller=user&action=login">
+                                <form id="loginForm" name="loginForm" role="form" style="display: block;" method="post" action="index.php?controller=user&action=register">
+                                    <div class="form-group">
+                                        <input type="name" name="firstName" id="firstName" tabindex="3" class="form-control" placeholder="First Name" value=""  required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="name" name="lastName" id="lastName" tabindex="4" class="form-control" placeholder="Last Name" value=""  required>
+                                    </div>
                                     <div class="form-group">
                                         <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email" value=""  required>
                                     </div>
@@ -20,11 +25,11 @@
                                     </div>
                                     <div class="col-xs-12 form-group pull-right">     
                                         <button type="submit" name="loginSubmit" id="loginSubmit" tabindex="3" class="form-control btn btn-warning">
-                                        <span class="spinner"><i class="icon-spin icon-refresh" id="spinner"></i></span> Log In
+                                            <span class="spinner"><i class="icon-spin icon-refresh" id="spinner"></i></span> Register
                                         </button>
                                     </div>
                                 </form>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -32,4 +37,4 @@
         </div>
     </div>
 </body>
-</html>
+
