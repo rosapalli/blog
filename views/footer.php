7fc1c7c398909ce14ad73f86d2ac3dd9fcec6bd9
ErikoRebeccaKobayashi <kornonkobs@hotmail.com>
2019-04-30 00:36:23 +0100

    uploaded footer additions in views/css/styles, views/footer and views/layout from Dee

---
+++ views/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<footer>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="container">
+    <p>This website was made in 2019.</p>
+    <container class="row">
+        <div class="column"><a href = "">Terms and Conditions</a></div>
+        <div class="column"><a href = "">Cookie Policy</a></div>
+        <div class="column"><a href = "">Contact Us</a></div>
+        <div class="column"><a href = "">Facebook</a></div>
+        <div class="column"><a href = "">Twitter</a></div>
+        <div class="column"><a href = "">Instagram</a></div>
+    </container>
+    </div>
+</footer> 
+
